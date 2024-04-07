6d8a191eac334e2b6e2306d931465602410ed77c
Riley-study <120293475+Riley-study@users.noreply.github.com>
2024-04-07 19:42:41 +0500

    seminar 5 added

--- myproject/gameapp/admin.py
+++ myproject/gameapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Coin
 
 # Register your models here.
+
+class CoinAdmin(admin.ModelAdmin):
+    list_display = ('size',)
+
+admin.site.register(Coin, CoinAdmin)
+
--- myproject/storageapp/admin.py
+++ myproject/storageapp/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
+from .models import Client, Product, Order
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    # Настройка полей, которые будет видно в полном списке
+    list_display = ('name', 'email', 'phone_number',)
+    # Настройка полей, по которым можно фильтровать значения
+    list_filter = ('name', 'email', 'phone_number')
+    # Настройка полей, по которым можно будет искать значения
+    search_fields = ('name', 'email', 'phone_number')
+    # Настройка полей с разбивкой на подразделы
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ['name'],
+            },
+        ),
+        (
+            'Контактные данные клиента',
+            {
+                'fields': ['email', 'phone_number', 'address'],
+            }
+        ),
+    ]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'date_added',)
+    list_filter = ('name', 'price', 'quantity', 'date_added',)
+    search_fields = ('name', 'price', 'quantity', 'date_added',)
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ['name'],
+            },
+        ),
+        (
+            'Описание товара',
+            {
+                'classes': ['collapse'],
+                'description': 'Описание товара',
+                'fields': ['description'],
+            },
+        ),
+        (
+            'На складе',
+            {
+                'fields': ['price', 'quantity', 'date_added'],
+            }
+        ),
+    ]
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('client', 'total_amount', 'order_date',)
+    list_filter = ('client', 'total_amount', 'order_date',)
+    search_fields = ('client', 'total_amount', 'order_date',)
+    fields = ['client', 'products', 'total_amount', 'order_date', ]
+    readonly_fields = ['order_date', ]
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
